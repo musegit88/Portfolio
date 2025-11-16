e5450d543312121949e6aaf3f52a4efa61a2f816
Muse <musemy8484@gmail.com>
2025-11-16 15:09:47 +0300

    style: backdrop blur added to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,20 +4,24 @@ import { ThemeToggle } from "./theme-toggle";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between p-4">
-      <Link href={"/"} className="flex items-center">
-        <div className="bg-black border rounded-tl-md rounded-bl-md text-white p-2 font-semibold">
-          M4
+    <div className="fixed w-full top-0 left-0 z-50 bg-white/80 dark:bg-black/10 backdrop-blur-sm">
+      <div className="container">
+        <div className="flex items-center justify-between p-4">
+          <Link href={"/"} className="flex items-center">
+            <div className="bg-black border rounded-tl-md rounded-bl-md text-white p-2 font-semibold">
+              M4
+            </div>
+            <div className="border rounded-tr-md rounded-br-md p-2  font-semibold shadow-md">
+              Dev
+            </div>
+          </Link>
+          <div className="flex items-center gap-4">
+            <a href="https://github.com/musegit88" target="_blank">
+              <FaGithub size={20} />
+            </a>
+            <ThemeToggle />
+          </div>
         </div>
-        <div className="border rounded-tr-md rounded-br-md p-2  font-semibold shadow-md">
-          Dev
-        </div>
-      </Link>
-      <div className="flex items-center gap-4">
-        <a href="https://github.com/musegit88" target="_blank">
-          <FaGithub size={20} />
-        </a>
-        <ThemeToggle />
       </div>
     </div>
   );
