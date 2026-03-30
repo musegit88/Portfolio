1788af166bec5813dff401c0fdc47900fe01d255
Muse <musemy8484@gmail.com>
2026-03-30 10:13:59 +0300

    style:add space & correct typo

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="mt-20 flex flex-col items-center md:flex-row  w-full">
       <div className="flex flex-col gap-8 max-w-2xl">
         <h2 className="max-sm:text-3xl text-4xl font-semibold">
-          Hi,I&apos;M Muse a full-stack <br /> web developer{" "}
+          Hi, I&apos;m Muse a full-stack <br /> web developer{" "}
         </h2>
         <p className="tracking-tighter max-[932px]:max-w-[480px] text-lg duration-500 animate animate-in fade-in-5 slide-in-from-bottom-2.5">
           {" "}
