3b96ba0f3fcdfeff94e95856a3debf8e17fe8f16
Muse <musemy8484@gmail.com>
2026-05-22 15:49:10 +0300

    fix: create form component

--- app/admin/(admin_routes)/dashboard/projects/add/page.tsx
+++ app/admin/(admin_routes)/dashboard/projects/add/page.tsx
@@ -1,24 +1,3 @@
-"use client";
-
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  Field,
-  FieldGroup,
-  FieldLabel,
-  FieldLegend,
-  FieldSet,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import {
-  MultiSelect,
-  MultiSelectContent,
-  MultiSelectGroup,
-  MultiSelectItem,
-  MultiSelectTrigger,
-  MultiSelectValue,
-} from "@/components/ui/multi-select";
-import { Textarea } from "@/components/ui/textarea";
-import { webDevTechs } from "@/lib/constants";
 import Form from "../../../_components/form";
 
 const CreateProjectPage = () => {
