7bed3c721d4e787564894fb4b6eacc47696e6c9c
Muse <musemy8484@gmail.com>
2026-05-27 01:20:41 +0300

    feat: add empty messages if there is no projects or skills

--- app/admin/(admin_routes)/dashboard/page.tsx
+++ app/admin/(admin_routes)/dashboard/page.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import { getServerSession } from "next-auth";
+import { Cog, FolderPlus, Plus } from "lucide-react";
+
 import { prisma } from "@/lib/prisma";
+import { navTabs } from "@/lib/constants";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
 import Overview from "../_components/overview";
 import Projects from "../_components/projects";
 import Skills from "../_components/skills";
 import AboutTab from "../_components/about";
-import { navTabs } from "@/lib/constants";
 
 const Dashboard = async () => {
   const session = await getServerSession();
@@ -53,10 +57,48 @@ const Dashboard = async () => {
             />
           </TabsContent>
           <TabsContent value="projects">
-            <Projects projects={projects} />
+            {projects.length > 0 ? (
+              <Projects projects={projects} />
+            ) : (
+              <div className="mt-24 flex flex-col items-center justify-center gap-4">
+                <div className="p-4 rounded-full bg-muted">
+                  <FolderPlus />
+                </div>
+                <h4 className="text-xl font-semibold">No projects yet</h4>
+                <p className="text-muted-foreground text-center">
+                  You haven&apos;t added any projects yet. Get started by adding
+                  your first project.
+                </p>
+                <Button asChild className="mb-4" title="Add Project">
+                  <Link href="/admin/projects/add">
+                    <Plus className="mr-2" />
+                    Add Project
+                  </Link>
+                </Button>
+              </div>
+            )}
           </TabsContent>
           <TabsContent value="skills">
-            <Skills skills={skills} />
+            {skills.length > 0 ? (
+              <Skills skills={skills} />
+            ) : (
+              <div className="mt-24 flex flex-col items-center justify-center gap-4">
+                <div className="p-4 rounded-full bg-muted">
+                  <Cog />
+                </div>
+                <h4 className="text-xl font-semibold">No skills yet</h4>
+                <p className="text-muted-foreground text-center">
+                  You haven&apos;t added any skills yet. Get started by adding
+                  your first skill.
+                </p>
+                <Button asChild className="mb-4" title="Add Skill">
+                  <Link href="/admin/skills/add">
+                    <Plus className="mr-2" />
+                    Add Skill
+                  </Link>
+                </Button>
+              </div>
+            )}
           </TabsContent>
           <TabsContent value="about" className="w-full">
             <AboutTab about={about} />
