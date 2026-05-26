78ed55a5f5f6547f363371c428ec1ce648cf6ea5
Muse <musemy8484@gmail.com>
2026-05-27 01:10:50 +0300

    feat: use avatarUrl form about and layout provide session to header

--- app/admin/(admin_routes)/layout.tsx
+++ app/admin/(admin_routes)/layout.tsx
@@ -1,14 +1,22 @@
-import React from "react";
-import Header from "./_components/header";
 import { getServerSession } from "next-auth";
+
+import { prisma } from "@/lib/prisma";
+
+import Header from "./_components/header";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession();
   if (!session) return;
 
+  const avatar = await prisma.about.findFirst({
+    select: {
+      avatarUrl: true,
+    },
+  });
+
   return (
     <div>
-      <Header session={session} />
+      <Header session={session} avatar={avatar?.avatarUrl || undefined} />
       <main className="container my-4">{children}</main>
     </div>
   );
